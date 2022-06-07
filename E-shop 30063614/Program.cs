9cd060cfe16d159454f4be641fa68dd6d0ba1ff3
sketcherz1811 <30063614@student.toiohomai.ac.nz>
2022-06-07 19:17:25 +1200

    Revert "name input and main menu done"
    This reverts commit 1592119f49fa105d97969e0b7d9a40b7f14d3e0a.

--- E-shop 30063614/Program.cs
+++ E-shop 30063614/Program.cs
@@ -8,39 +8,19 @@ namespace E_shop_30063614
 {
     internal class Program
     {
-        // aray of what the user is allowed too input 
-        public static string[] menuOptions = { "1", "2", "3", "4", "5" };
-
-        // ballace
-        public static decimal balance = 20000;
-
-        // option that the user has selected 
-        public static string userOption = "";
-
-        private static void CheckPin()
-        {
-            string Name = "";
-            Console.WriteLine("*****************************************************************************");
-            Console.WriteLine("\t            Welcome too Appliace plus\n");
-            Console.WriteLine("                 \tPlease enter your name");
-            Console.WriteLine("*****************************************************************************");
-            // User input
-            Name = Console.ReadLine();
-            MainMenu();
-
-        }
+        public static string UserName;
 
         static void MainMenu()
         {
-            Console.WriteLine("****************************************\n");
-            Console.WriteLine("\tPlease select an Option:\n");
-            Console.WriteLine("\t [1] View Balance");
-            Console.WriteLine("\t [2] TV's");
-            Console.WriteLine("\t [3] Fridges");
-            Console.WriteLine("\t [4] Dishwashers");
-            Console.WriteLine("\t [5] Exit Application\n");
-            Console.WriteLine("****************************************\n");
-        }
+            Console.WriteLine("welcome to Appliance Plus,please enter your name");
+            UserName = Console.ReadLine();
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("welcome " + UserName + " please select an option from the menu");
+            Console.WriteLine("(1) check balance /n (2) televisions /n (3) Washing machines /n (4) fridges /n");
+                }
+        static void Main(string[] args)
+        {//start of main input
+            MainMenu();
+        }//end of main input
     }
 }
-
